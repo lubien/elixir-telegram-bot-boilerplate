c580ca439a6ccd24b88e22076604f9183851158f
lubien <lubien1996@gmail.com>
2017-11-26 00:31:26 -0300

    [Fixes #6] Warn when bot_name is missing

--- lib/app.ex
+++ lib/app.ex
@@ -2,6 +2,19 @@ defmodule App do
   use Application
 
   def start(_type, _args) do
+    bot_name = Application.get_env(:app, :bot_name)
+
+    unless String.valid?(bot_name) do
+      IO.warn """
+      Env not found Application.get_env(:app, :bot_name)
+      This will give issues when generating commands
+      """
+    end
+
+    if bot_name == "" do
+      IO.warn "An empty bot_name env will make '/anycommand@' valid"
+    end
+
     import Supervisor.Spec, warn: false
 
     children = [
